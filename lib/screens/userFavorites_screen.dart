6c30deb2d52d0835c4e15080844eba98f85120b8
beyzapp <beyzapelit375@gmail.com>
2025-04-30 21:10:27 +0300

    Update userFavorites_screen.dart

--- lib/screens/userFavorites_screen.dart
+++ lib/screens/userFavorites_screen.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:nom_nom_guide/manager/favorites.dart';
 
-class FavoritesScreen extends StatelessWidget {
-  const FavoritesScreen({super.key});
+class FavoritesPlacesScreen extends StatelessWidget {
+  const FavoritesPlacesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
