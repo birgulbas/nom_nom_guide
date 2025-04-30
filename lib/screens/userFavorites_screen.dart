c0806905580098e0fc9950382c0d0b1566b5a032
beyzapp <beyzapelit375@gmail.com>
2025-04-30 21:05:56 +0300

    Update userFavorites_screen.dart

--- lib/screens/userFavorites_screen.dart
+++ lib/screens/userFavorites_screen.dart
@@ -1,18 +1,33 @@
+//kullanıcıya göre çekiyor favorileri
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:nom_nom_guide/manager/favorites.dart';
 
-class FavoritesPlacesScreen extends StatelessWidget {
-  const FavoritesPlacesScreen({super.key});
+class FavoritesScreen extends StatelessWidget {
+  const FavoritesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final favoritesManager = Provider.of<FavoritesManager>(context);
+    final favorites = favoritesManager.favorites;
+
     return Scaffold(
-        appBar: AppBar(
-  title: const Text('Favorites'),
-  backgroundColor: Colors.pink.shade600, 
-),
-      body: Center(
-        child: Text("Favori mekanlar burada görüntülenecek."),
-      ),
+      appBar: AppBar(title: const Text("FAVORITES")),
+      body: favorites.isEmpty
+          ? const Center(
+              child: Text("Favoriler burada listelenecek."),
+            )
+          : ListView.builder(
+              itemCount: favorites.length,
+              itemBuilder: (context, index) {
+                final place = favorites[index];
+                return ListTile(
+                  title: Text(place.name),
+                  subtitle: Text(place.location),
+                );
+              },
+            ),
     );
   }
 }
+
